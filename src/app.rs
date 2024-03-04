cf17b64c22281559a7700298c6ab2df8bbb255b0
Dhaval Savalia <hello@dhavalsavalia.com>
2024-03-04 12:41:59 -0500

    translate OTP button

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,20 @@
 use enigo::Enigo;
 use log::info;
+use rust_i18n::t;
 use MessAuto::{enter, paste};
 
 slint::include_modules!();
 
 pub fn main(code: &str, from_app: &str) -> Result<(), slint::PlatformError> {
+    let paste_code_instruction = t!("paste_code_instruction");
+    let verification_code = t!("verification-code");
+    let from_label = t!("from-label");
     let ui = AppWindow::new()?;
     ui.set_code(code.into());
     ui.set_from_app(from_app.into());
+    ui.set_paste_code_instruction(paste_code_instruction.into());
+    ui.set_verification_code(verification_code.into());
+    ui.set_from_label(from_label.into());
     let mut enigo = Enigo::new();
 
     let ui_handle = ui.as_weak();
