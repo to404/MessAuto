f0a6a886d694ae60da8d6395da77b08737eb5f80
Dhaval Savalia <hello@dhavalsavalia.com>
2024-03-04 12:49:05 -0500

    Refactor code to use verification code label instead of separate properties

--- src/app.rs
+++ src/app.rs
@@ -7,14 +7,16 @@ slint::include_modules!();
 
 pub fn main(code: &str, from_app: &str) -> Result<(), slint::PlatformError> {
     let paste_code_instruction = t!("paste_code_instruction");
-    let verification_code = t!("verification-code");
-    let from_label = t!("from-label");
+    let verification_code_label = format!(
+        "{}: {}\n{} {}",
+        t!("verification-code"),
+        code,
+        t!("from-label"),
+        from_app
+    );
     let ui = AppWindow::new()?;
-    ui.set_code(code.into());
-    ui.set_from_app(from_app.into());
     ui.set_paste_code_instruction(paste_code_instruction.into());
-    ui.set_verification_code(verification_code.into());
-    ui.set_from_label(from_label.into());
+    ui.set_verification_code_label(verification_code_label.into());
     let mut enigo = Enigo::new();
 
     let ui_handle = ui.as_weak();
